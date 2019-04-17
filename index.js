50d3b98e505f0a5533dd4facf75cd59ae3427dd9
valentin <valentin@forum.eu>
2019-04-17 17:41:44 +0200

    config

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-class DexBlue{
+let Web3 = require('web3'),
+    web3 = new Web3();
 
+class DexBlueUtils{
+
+}
+
+class DexBlue{
+    constructor(parameters){
+        this.utils = new DexBlueUtils()
+    }
 }
 
 module.exports = dexBlue
